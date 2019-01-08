6b2bf14ae6e0ab8631ea7f5c3461a28cb096819d
Philippe DEMANGET <philippe.demanget-ext@afs2r.fr>
2019-01-08 14:04:08 +0100

    Router links

--- javascript/angular/forms/src/app/app.module.ts
+++ javascript/angular/forms/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DemoComponent } from './demo/demo.component';
+import { Routes, RouterModule } from '@angular/router';
 
+
+const appRoutes: Routes = [
+
+  { path: 'demo', component: DemoComponent },
+
+  { path: '', component: HomeComponent }
+
+];
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
