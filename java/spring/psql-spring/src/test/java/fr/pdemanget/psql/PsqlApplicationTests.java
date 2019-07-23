51a4ef0a72136e896ee7756e70431568ab328116
Philippe Demanget <philippe.demanget-ext@afs2r.fr>
2019-07-23 23:33:21 +0200

    psql-spring

---
+++ java/spring/psql-spring/src/test/java/fr/pdemanget/psql/PsqlApplicationTests.java
@@ -0,0 +1,17 @@
+package fr.pdemanget.psql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PsqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
